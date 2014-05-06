10c9299353aee6c7372e40b7a5427c3e2c718e8a
DSH105 <jason.chirgwin@icloud.com>
2014-05-06 16:14:22 +1000

    Fix the mess I made (thanks to eueln <3)

---
+++ src/main/java/com/dsh105/nexus/command/module/ChannelStatsCommand.java
@@ -0,0 +1,25 @@
+package com.dsh105.nexus.command.module;
+
+import com.dsh105.nexus.command.Command;
+import com.dsh105.nexus.command.CommandModule;
+import com.dsh105.nexus.command.CommandPerformEvent;
+import org.pircbotx.Channel;
+
+@Command(command = "chanstats", needsChannel = true, help = "Gives stats on the current channel.",
+        extendedHelp = {"Gives the number of users connected plus some useful metrics on voiced/opped users."})
+public class ChannelStatsCommand extends CommandModule {
+    @Override
+    public boolean onCommand(CommandPerformEvent event) {
+        Channel chan = event.getChannel();
+        String chanName = chan.getName();
+        int numUsers = chan.getUsers().size();
+        int numOpped = chan.getOps().size();
+        int numVoiced = chan.getVoices().size();
+
+        int opPercentage = Math.round(((float) numOpped / numUsers) * 100);
+        int voicePercentage = Math.round(((float) numVoiced / numUsers) * 100);
+
+        event.respond(String.format("Channel %s has %d users. There are %d ops (%d%%) and %d voiced users (%d%%).", chanName, numUsers, numOpped, opPercentage, numVoiced, voicePercentage));
+        return true;
+    }
+}
