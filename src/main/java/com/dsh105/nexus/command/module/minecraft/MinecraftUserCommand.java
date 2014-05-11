9f11b4d837eea4c42a76a8e9d63d06f5c8e6d350
Luke Anderson (stuntguy3000) <stuntguy3000@gmail.com>
2014-05-12 07:43:37 +0930

    Remove UUIDFetcher and JSON-Simple

--- src/main/java/com/dsh105/nexus/command/module/minecraft/MinecraftUserCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/minecraft/MinecraftUserCommand.java
@@ -3,12 +3,13 @@ package com.dsh105.nexus.command.module.minecraft;
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
-import com.dsh105.nexus.util.minecraft.UUIDFetcher;
+import com.dsh105.nexus.util.JsonUtil;
 import org.pircbotx.Colors;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -25,7 +26,7 @@ public class MinecraftUserCommand extends CommandModule {
         if (event.getArgs().length == 1) {
             String username = event.getArgs()[0];
             if (!username.matches("^([A-Za-z]|[0-9]|_)+$")) {
-                event.respondWithPing(Colors.RED + "Minecraft usernames can only contain Letters, Numbers and Underscores!");
+                event.respondWithPing(Colors.RED + "Minecraft usernames can only contain letters, numbers and underscores!");
                 return true;
             }
 
@@ -33,7 +34,10 @@ public class MinecraftUserCommand extends CommandModule {
             boolean isValid;
             boolean hasPaid;
             try {
-                uuid = UUIDFetcher.getUUIDOf(username).toString();
+                JsonUtil jsonUtil = new JsonUtil();
+                UserProfile[] profile = jsonUtil.read(getUUIDConnection(username), "profiles", UserProfile[].class);
+
+                uuid = profile[0].id;
                 isValid = getPageContents("https://account.minecraft.net/buy/frame/checkName/" + username).equalsIgnoreCase("TAKEN");
                 hasPaid = getPageContents("https://minecraft.net/haspaid.jsp?user=" + username).equalsIgnoreCase("true");
             } catch (Exception e) {
@@ -47,7 +51,7 @@ public class MinecraftUserCommand extends CommandModule {
                     Colors.BOLD + "Valid Username? " + (isValid ?
                             Colors.BOLD + Colors.UNDERLINE + Colors.GREEN + "Yes" :
                             Colors.BOLD + Colors.UNDERLINE + Colors.RED + "No") + Colors.NORMAL + " | "
-                            + Colors.BOLD + "Paid Account? " + (isValid ?
+                            + Colors.BOLD + "Paid Account? " + (hasPaid ?
                             Colors.BOLD + Colors.UNDERLINE + Colors.GREEN + "Yes" :
                             Colors.BOLD + Colors.UNDERLINE + Colors.RED + "No")
             );
@@ -79,5 +83,31 @@ public class MinecraftUserCommand extends CommandModule {
         input.close();
 
         return page;
+    }
+
+    private BufferedReader getUUIDConnection(String username) throws IOException {
+        URL url = new URL("https://api.mojang.com/profiles/page/1");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        String JSON = String.format("[{\"name\":\"%s\", \"agent\":\"Minecraft\"}]", username);
+
+        OutputStream stream = connection.getOutputStream();
+        stream.write(JSON.getBytes());
+        stream.flush();
+        stream.close();
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        return input;
+    }
+
+    private class UserProfile {
+        private String id;
+        private String name;
     }
 }
