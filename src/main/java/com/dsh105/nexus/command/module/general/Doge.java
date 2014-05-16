34c248a2319b8a3f8f70eb808f155e44075e2ac8
JOPHESTUS <JOPHESTUS@jophest.us>
2014-05-16 22:24:41 +1000

    added dogecoin currency conversions

---
+++ src/main/java/com/dsh105/nexus/command/module/general/Doge.java
@@ -0,0 +1,54 @@
+package com.dsh105.nexus.command.module.general;
+
+
+import com.dsh105.nexus.command.Command;
+import com.dsh105.nexus.command.CommandModule;
+import com.dsh105.nexus.command.CommandPerformEvent;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+import org.pircbotx.Colors;
+
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
+
+@Command(command = "doge", needsChannel = false, help = "Doge currency converter",
+        extendedHelp = {"{b}{p}{c}{/b} <value> - Converts the entered value to either doge or usd."})
+public class Doge extends CommandModule {
+    @Override
+    public boolean onCommand(CommandPerformEvent event) {
+        if (event.getArgs().length == 0) {
+            return false;
+        } else {
+            try {
+                HttpResponse<JsonNode> jsonResponse = Unirest.get("https://www.dogeapi.com/wow/v2/")
+                        .field("a", "get_info")
+                        .header("accept", "application/json")
+                        .asJson();
+                                double dogeyer = jsonResponse.getBody().getObject().getJSONObject("data").getJSONObject("info").getDouble("doge_usd");
+                double amtInUSDtoDoge = Double.parseDouble(event.getArgs()[0]);
+                amtInUSDtoDoge = amtInUSDtoDoge / dogeyer;
+
+                double amtInDogetoUSD = Double.parseDouble(event.getArgs()[0]);
+                amtInDogetoUSD = amtInDogetoUSD * dogeyer;
+
+                amtInDogetoUSD = (double) Math.round(amtInDogetoUSD * 100) / 100;
+                amtInUSDtoDoge = (double) Math.round(amtInUSDtoDoge * 100) / 100;
+
+                event.respond(Colors.BOLD + "USD --> Doge: " + Colors.NORMAL + Colors.UNDERLINE + amtInUSDtoDoge);
+                event.respond(Colors.BOLD + "Doge --> USD: " + Colors.NORMAL + Colors.UNDERLINE + amtInDogetoUSD);
+
+
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+    }
+}
