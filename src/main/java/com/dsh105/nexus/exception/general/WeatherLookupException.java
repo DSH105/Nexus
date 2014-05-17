e98faed3096bb920620d12068417ccc0a77a3148
JOPHESTUS <JOPHESTUS@jophest.us>
2014-05-17 18:09:12 +1000

    added weather command

---
+++ src/main/java/com/dsh105/nexus/exception/general/WeatherLookupException.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Nexus.
+ *
+ * Nexus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nexus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nexus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.nexus.exception.general;
+
+public class WeatherLookupException extends RuntimeException {
+
+    public WeatherLookupException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+}
