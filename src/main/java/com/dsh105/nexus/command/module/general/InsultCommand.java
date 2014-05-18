fd74c7efb21adde0cb9293484eb2d7027dca34a8
JOPHESTUS <JOPHESTUS@jophest.us>
2014-05-18 15:10:15 +1000

    fixed spacing before lol768 gets a chance to complain about it

--- src/main/java/com/dsh105/nexus/command/module/general/InsultCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/general/InsultCommand.java
@@ -3,21 +3,10 @@ package com.dsh105.nexus.command.module.general;
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
-import com.dsh105.nexus.exception.bukkit.BukkitUserException;
 import com.dsh105.nexus.exception.general.InsultLookupException;
-import com.dsh105.nexus.util.shorten.URLShortener;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.ocpsoft.prettytime.PrettyTime;
-import org.pircbotx.Colors;
-
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Command(command = "insult", aliases = {}, needsChannel = false, help = "Insult generator",
         extendedHelp = {"{b}{p}{c}{/b} - Prints an insult"})
@@ -27,7 +16,6 @@ public class InsultCommand extends CommandModule {
 
     @Override
     public boolean onCommand(CommandPerformEvent event) {
-
         try {
             Document doc = Jsoup.connect(INSULT_URL).get();
             Element insult = doc.select("td").first();
@@ -40,7 +28,6 @@ public class InsultCommand extends CommandModule {
         } catch (Exception e) {
             throw new InsultLookupException("An error occurred while trying to retrieve the information", e);
         }
-
         return true;
     }
 }
