531e0b438c29109f1e1d630665fc856db1b9b375
lol768 <lol768@users.noreply.github.com>
2014-05-18 03:48:19 +0100

    Add GlareCommand

---
+++ src/main/java/com/dsh105/nexus/command/module/action/GlareCommand.java
@@ -0,0 +1,11 @@
+package com.dsh105.nexus.command.module.action;
+
+import com.dsh105.nexus.command.Command;
+
+@Command(command = "glare", needsChannel = false, help = "glare at a user",
+        extendedHelp = {"{b}{p}{c}{/b} <user> - glare at someone!"})
+public class GlareCommand extends AbstractActionCommand {
+    public GlareCommand() {
+        this.setVerb("glare");
+    }
+}
