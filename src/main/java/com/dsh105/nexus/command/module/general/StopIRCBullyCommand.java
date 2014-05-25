42f2a33593c756144e755a3ddafa360e7496c49d
Luke Anderson (stuntguy3000) <stuntguy3000@gmail.com>
2014-05-25 20:40:42 +0930

    Make command work

--- src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
@@ -4,24 +4,20 @@ import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
 
-
 @Command(command = "sib",
         aliases = {"stopb"},
         needsChannel = false,
         help = "Stops IRC Bullying",
-        extendedHelp = {"{b}{p}{c}{/b} <target> - Tells the target to stop bullying you."})
-
+        extendedHelp = {
+                "{b}{p}{c}{/b} <target> - Tells the target to stop bullying you."
+        })
 public class StopIRCBullyCommand extends CommandModule {
-    
-
     @Override
     public boolean onCommand(CommandPerformEvent event) {
-
         if (event.getArgs().length == 1) {
-            event.respondWithPing(event.getSender().getNick() + " feels offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
-
+            event.respond("({0}) {1} feels offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop", event.getArgs()[0], event.getSender().getNick());
             return true;
-        }else{
+        } else {
             return false;
         }
     }
