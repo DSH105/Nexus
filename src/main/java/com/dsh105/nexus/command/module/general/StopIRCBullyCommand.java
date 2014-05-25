7aad5b1b555a44746d715073310ec1866136f14a
JOPHESTUS <JOPHESTUS@jophest.us>
2014-05-25 21:38:52 +1000

    removed unused imports

--- src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
@@ -3,10 +3,6 @@ package com.dsh105.nexus.command.module.general;
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
-import com.dsh105.nexus.exception.general.InsultLookupException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 @Command(command = "sib",
         aliases = {"stopb"},
@@ -14,16 +10,12 @@ import org.jsoup.nodes.Element;
         help = "Stops IRC Bullying",
         extendedHelp = {"{b}{p}{c}{/b} <target> - Tells the target to stop bullying you."})
 
-public class StopIRCBullyCommand extends CommandModule {
-    
-
+public class StopIRCBullyCommand extends CommandModule {  
     @Override
     public boolean onCommand(CommandPerformEvent event) {
-
         if (event.getArgs().length == 1) {
             event.respond("(" + event.getArgs()[0] + ") " + event.getSender().getNick() + " feels offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
-
-            return true;
+         return true;
         }else{
             return false;
         }
