83b87932855d700a88423360700abd0c7f3e42e2
JOPHESTUS <JOPHESTUS@jophest.us>
2014-05-25 21:37:48 +1000

    changes

--- src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/general/StopIRCBullyCommand.java
@@ -3,21 +3,28 @@ package com.dsh105.nexus.command.module.general;
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
+import com.dsh105.nexus.exception.general.InsultLookupException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 @Command(command = "sib",
         aliases = {"stopb"},
         needsChannel = false,
         help = "Stops IRC Bullying",
-        extendedHelp = {
-                "{b}{p}{c}{/b} <target> - Tells the target to stop bullying you."
-        })
+        extendedHelp = {"{b}{p}{c}{/b} <target> - Tells the target to stop bullying you."})
+
 public class StopIRCBullyCommand extends CommandModule {
+    
+
     @Override
     public boolean onCommand(CommandPerformEvent event) {
+
         if (event.getArgs().length == 1) {
-            event.respond(event.getArgs()[0] + ": " + event.getSender().getNick() + " feels offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
+            event.respond("(" + event.getArgs()[0] + ") " + event.getSender().getNick() + " feels offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop");
+
             return true;
-        } else {
+        }else{
             return false;
         }
     }
