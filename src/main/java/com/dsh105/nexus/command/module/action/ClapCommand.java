810ed4bfdf46f7394380138f49399f3fa3ebd716
lol768 <lol768@users.noreply.github.com>
2014-05-25 12:45:08 +0100

    I contributed!

---
+++ src/main/java/com/dsh105/nexus/command/module/action/ClapCommand.java
@@ -0,0 +1,31 @@
+/*
+* This file is part of Nexus.
+*
+* Nexus is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Nexus is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Nexus. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.dsh105.nexus.command.module.action;
+
+import com.dsh105.nexus.command.Command;
+
+@Command(command = "poke",
+        needsChannel = false,
+        helpGroups = "action",
+        help = "Clap",
+        extendedHelp = {"{b}{p}{c}{/b} <user> - applaud someone!"})
+public class ClapCommand extends AbstractActionCommand {
+    public ClapCommand() {
+        this.setVerb("clap");
+    }
+}
