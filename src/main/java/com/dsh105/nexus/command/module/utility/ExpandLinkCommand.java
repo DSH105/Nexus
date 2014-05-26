adf7acaa16653d03d9c8bcafeb0a22966c863581
DSH105 <jason.chirgwin@icloud.com>
2014-05-26 16:14:26 +1000

    Create utility and information module packages

--- src/main/java/com/dsh105/nexus/command/module/utility/ExpandLinkCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/utility/ExpandLinkCommand.java
@@ -15,37 +15,48 @@
  * along with Nexus.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.dsh105.nexus.command.module.general;
+package com.dsh105.nexus.command.module.utility;
 
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
-import java.util.UUID;
-
-@Command(command = "uuid",
+@Command(command = "expand",
+        aliases = {"expandlink"},
         needsChannel = false,
-        help = "Validate a UUID.",
+        help = "See where a shortened URL goes too",
         extendedHelp = {
-                "{b}{p}{c}{/b} <input> - Tests if input is a valid UUID."
+                "{b}{p}{c}{/b} <url> - See where a shortened URL goes too."
         })
-public class UUIDCommand extends CommandModule {
+public class ExpandLinkCommand extends CommandModule {
+
+    public static final String API_URL = "http://api.longurl.org/v2/expand?format=json&url=";
 
     @Override
     public boolean onCommand(CommandPerformEvent event) {
         String[] args = event.getArgs();
-
         if (args.length != 1) {
             return false;
         }
 
-        try {
-            UUID u = UUID.fromString(args[0]);
-            event.respondWithPing("{0} is a valid UUID!", args[0]);
-        } catch (Exception ex) {
-            event.respondWithPing("{0} is not a valid UUID!", args[0]);
-        }
+        String url;
 
+        try {
+            url = args[0];
+            HttpResponse<JsonNode> response = Unirest.post(API_URL + url).asJson();
+
+            String longUrl = response.getBody().getObject().getString("long-url");
+            if (longUrl.equalsIgnoreCase(url)) {
+                event.respondWithPing("{0} has no redirects!", url);
+            } else {
+                event.respondWithPing("{0} → {1}.", url, longUrl);
+            }
+        } catch (Exception e) {
+            event.errorWithPing("An error occurred! Is it a valid URL?");
+        }
         return true;
     }
 }
--- src/main/java/com/dsh105/nexus/command/module/utility/UUIDCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/utility/UUIDCommand.java
@@ -15,48 +15,37 @@
  * along with Nexus.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.dsh105.nexus.command.module.general;
+package com.dsh105.nexus.command.module.utility;
 
 import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
 
-@Command(command = "expand",
-        aliases = {"expandlink"},
+import java.util.UUID;
+
+@Command(command = "uuid",
         needsChannel = false,
-        help = "See where a shortened URL goes too",
+        help = "Validate a UUID.",
         extendedHelp = {
-                "{b}{p}{c}{/b} <url> - See where a shortened URL goes too."
+                "{b}{p}{c}{/b} <input> - Tests if input is a valid UUID."
         })
-public class ExpandLinkCommand extends CommandModule {
-
-    public static final String API_URL = "http://api.longurl.org/v2/expand?format=json&url=";
+public class UUIDCommand extends CommandModule {
 
     @Override
     public boolean onCommand(CommandPerformEvent event) {
         String[] args = event.getArgs();
+
         if (args.length != 1) {
             return false;
         }
 
-        String url;
-
         try {
-            url = args[0];
-            HttpResponse<JsonNode> response = Unirest.post(API_URL + url).asJson();
-
-            String longUrl = response.getBody().getObject().getString("long-url");
-            if (longUrl.equalsIgnoreCase(url)) {
-                event.respondWithPing("{0} has no redirects!", url);
-            } else {
-                event.respondWithPing("{0} → {1}.", url, longUrl);
-            }
-        } catch (Exception e) {
-            event.errorWithPing("An error occurred! Is it a valid URL?");
+            UUID u = UUID.fromString(args[0]);
+            event.respondWithPing("{0} is a valid UUID!", args[0]);
+        } catch (Exception ex) {
+            event.respondWithPing("{0} is not a valid UUID!", args[0]);
         }
+
         return true;
     }
 }
