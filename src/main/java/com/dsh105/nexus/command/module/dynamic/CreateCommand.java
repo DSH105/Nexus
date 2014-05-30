75797d22d7e21a8531e2383251a0713854805f8a
DSH105 <jason.chirgwin@icloud.com>
2014-05-30 20:13:30 +1000

    Save dynamic commands after being created

--- src/main/java/com/dsh105/nexus/command/module/dynamic/CreateCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/dynamic/CreateCommand.java
@@ -66,6 +66,7 @@ public class CreateCommand extends CommandModule {
             CommandModule module = event.getManager().getModuleFor(command);
             if (module == null) {
                 event.errorWithPing("Aliases cannot be added to a command that does not exist ({0}).", command);
+                return true;
             }
             if (!(module instanceof DynamicCommand)) {
                 event.errorWithPing("Aliases cannot be added to {0}.", module.info().command());
@@ -79,6 +80,7 @@ public class CreateCommand extends CommandModule {
 
         DynamicCommand dynamicCommand = new DynamicCommandFactory().withCommand(command).withHelp(help).withExtendedHelp(help).withResponseOfType(response, type).prepare();
         event.getManager().register(dynamicCommand);
+        dynamicCommand.save();
         event.respondWithPing("Command registered as {0}", event.getCommandPrefix() + command);
         return true;
     }
