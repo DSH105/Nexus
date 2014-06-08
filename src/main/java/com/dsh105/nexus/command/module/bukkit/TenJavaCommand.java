6e85fb1babbe7c5267d5eb169aa46ef8b1cb93ee
DSH105 <jason.chirgwin@icloud.com>
2014-06-08 18:00:58 +1000

    Add \tj top command. Adds #89

--- src/main/java/com/dsh105/nexus/command/module/bukkit/TenJavaCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/bukkit/TenJavaCommand.java
@@ -21,6 +21,8 @@ import com.dsh105.nexus.command.Command;
 import com.dsh105.nexus.command.CommandModule;
 import com.dsh105.nexus.command.CommandPerformEvent;
 import com.dsh105.nexus.exception.bukkit.TenJavaDataLookupException;
+import com.dsh105.nexus.util.JsonUtil;
+import com.dsh105.nexus.util.StringUtil;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -40,28 +42,73 @@ import java.util.Date;
 public class TenJavaCommand extends CommandModule {
 
     public static final String TEN_JAVA_URL = "http://tenjava.com/assets/data.json";
+    public static final String TEN_JAVA_TOP_URL = "http://beta.tenjava.com/api/points";
+    public static final int TOP_LIMIT = 10;
     private final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     public boolean onCommand(CommandPerformEvent event) {
 
-        try {
-            HttpResponse<JsonNode> jsonResponse = Unirest.get(TEN_JAVA_URL)
-                    .header("accept", "application/json")
-                    .asJson();
-            int points = jsonResponse.getBody().getObject().getInt("points");
-            long time = jsonResponse.getBody().getObject().getLong("last_update");
-            PrettyTime pt = new PrettyTime();
-            time = time * 1000;
+        if (event.getArgs().length == 0) {
+            try {
+                HttpResponse<JsonNode> jsonResponse = Unirest.get(TEN_JAVA_URL)
+                        .header("accept", "application/json")
+                        .asJson();
+                int points = jsonResponse.getBody().getObject().getInt("points");
+                long time = jsonResponse.getBody().getObject().getLong("last_update");
+                PrettyTime pt = new PrettyTime();
+                time = time * 1000;
 
-            event.respond("Current points donated: " + Colors.BOLD + points + " ($" + df.format(points * 0.05) + " USD)" + Colors.NORMAL + ". Last updated: " + Colors.BOLD + pt.format(new Date(time)) + Colors.BOLD + " http://tenjava.com/points");
+                event.respond("Current points donated: " + Colors.BOLD + points + " ($" + df.format(points * 0.05) + " USD)" + Colors.NORMAL + ". Last updated: " + Colors.BOLD + pt.format(new Date(time)) + Colors.BOLD + " http://tenjava.com/points");
 
 
-        } catch (Exception e) {
-            throw new TenJavaDataLookupException("An error occurred in the lookup process", e);
+            } catch (Exception e) {
+                throw new TenJavaDataLookupException("An error occurred in the lookup process", e);
+            }
+
+        }
+
+        if (event.getArgs().length == 1 || event.getArgs().length == 2) {
+            if (event.getArgs()[0].equalsIgnoreCase("top")) {
+
+                int limit = TOP_LIMIT;
+                if (event.getArgs().length == 2 && StringUtil.isInt(event.getArgs()[1])) {
+                    limit = Integer.parseInt(event.getArgs()[1]);
+                }
+
+                try {
+                    TenJavaDonor[] donors = JsonUtil.read(Unirest.get(TEN_JAVA_TOP_URL).header("accept", "application/json"), "better_top", TenJavaDonor[].class);
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i <= limit; i++) {
+                        TenJavaDonor donor = donors[i];
+                        builder.append(Colors.BOLD + StringUtil.removePing(donor.getUsername()) + Colors.BOLD + " (" + donor.getAmount() + ")");
+                        if (i != limit) {
+                            builder.append(", ");
+                        }
+                    }
+
+                    event.respond(Colors.UNDERLINE + "Top " + limit + " donors: " + builder.toString());
+                    return true;
+                } catch (Exception e) {
+                    throw new TenJavaDataLookupException("An error occurred in the lookup process", e);
+                }
+            }
         }
 
         return true;
+    }
+
+    public class TenJavaDonor {
+        private int amount;
+        private String username;
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public String getUsername() {
+            return username;
+        }
     }
 }
 
