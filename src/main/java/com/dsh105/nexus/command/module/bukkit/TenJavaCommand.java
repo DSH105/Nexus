82b5b5e6114533e1e7b049c983e5a185f5364a28
DSH105 <jason.chirgwin@icloud.com>
2014-07-27 13:16:29 +1000

    Implement judging stats

--- src/main/java/com/dsh105/nexus/command/module/bukkit/TenJavaCommand.java
+++ src/main/java/com/dsh105/nexus/command/module/bukkit/TenJavaCommand.java
@@ -30,7 +30,10 @@ import org.ocpsoft.prettytime.PrettyTime;
 import org.pircbotx.Colors;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Command(command = "tenjava",
         aliases = {"tj"},
@@ -39,11 +42,14 @@ import java.util.Date;
         extendedHelp = {
                 "{b}{p}{c}{/b} - View tenjava donated points",
                 "{b}{p}{c} top{/b} - View the top donors to tenjava.",
-                "{b}{p}{c} top <number>{/b} - View the top donors to tenjava."
+                "{b}{p}{c} top <number>{/b} - View the top donors to tenjava.",
+                "{b}{p}{c} judge{/b} - View judging stats.",
+                "{b}{p}{c} judge <judge_name>{/b} - View stats for a certain judge."
         })
 public class TenJavaCommand extends CommandModule {
 
     public static final String TEN_JAVA_URL = "https://tenjava.com/api/points";
+    public static final String TEN_JAVA_TEAM_STATS_URL = "https://tenjava.com/api/team/stats";
     public static final int TOP_LIMIT = 10;
     private final DecimalFormat df = new DecimalFormat("0.00");
 
@@ -90,6 +96,39 @@ public class TenJavaCommand extends CommandModule {
                 } catch (Exception e) {
                     throw new TenJavaDataLookupException("An error occurred in the lookup process", e);
                 }
+            } else if (event.getArgs()[0].equalsIgnoreCase("judge")) {
+                TenJavaJudge[] judges = JsonUtil.read(Unirest.get(TEN_JAVA_TEAM_STATS_URL).header("accept", "application/json"), "judges", TenJavaJudge[].class);
+                String[] judgeNames = new String[judges.length];
+                for (int i = 0; i < judgeNames.length; i++) {
+                    judgeNames[i] = judges[i].getGithubUserName();
+                }
+                List<String> judgeNamesList = Arrays.asList(judgeNames);
+                if (event.getArgs().length == 2) {
+                    if (event.getArgs()[1].equalsIgnoreCase("list")) {
+                        event.respondWithPing("TenJava judges: " + StringUtil.buildSentenceList(judgeNames));
+                        return true;
+                    }
+
+                    String judgeName = event.getArgs()[1];
+                    if (judgeNamesList.contains(judgeName)) {
+                        TenJavaJudge judge = judges[judgeNamesList.indexOf(judgeName)];
+                        event.respondWithPing("Judging stats ({0}):" + judge.getGithubUserName());
+                        event.respondWithPing("Items: {0}/{1} ({2} remaining) - {3}", judge.getAssignedItems() + "", judge.getCompletedItems() + "", judge.getRemainingItems() + "", judge.getPercentComplete() + "%");
+                    } else {
+                        event.errorWithPing("Judge not found: {0}. Use {1} for a list of judges", judgeName, event.getCommandPrefix() + event.getCommand() + " judge list");
+                    }
+                    return true;
+                } else {
+                    int total = 0;
+                    int completed = 0;
+                    for (TenJavaJudge judge : judges) {
+                        total += judge.getAssignedItems();
+                        completed += judge.getCompletedItems();
+                    }
+                    int percentCompleted = (total / completed) * 100;
+                    event.respondWithPing("TenJava Judging stats: {0}/{1} items completed ({2})", completed + "", total + "", percentCompleted + "%");
+                    return true;
+                }
             }
         }
 
@@ -106,6 +145,39 @@ public class TenJavaCommand extends CommandModule {
 
         public String getUsername() {
             return username;
+        }
+    }
+
+    public class TenJavaJudge {
+        @SerializedName("github_username")
+        private String githubUserName;
+        @SerializedName("assignedItems")
+        private int assignedItems;
+        @SerializedName("completed_items")
+        private int completedItems;
+        @SerializedName("remaining_items")
+        private int remainingItems;
+        @SerializedName("percentage_complete")
+        private int percentComplete;
+
+        public String getGithubUserName() {
+            return githubUserName;
+        }
+
+        public int getAssignedItems() {
+            return assignedItems;
+        }
+
+        public int getCompletedItems() {
+            return completedItems;
+        }
+
+        public int getRemainingItems() {
+            return remainingItems;
+        }
+
+        public int getPercentComplete() {
+            return percentComplete;
         }
     }
 }
